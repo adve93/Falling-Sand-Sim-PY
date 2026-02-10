653e2db60759238a0dd3954dafcbfe4288690675
Afonso Soares <afonsopena93@gmail.com>
2026-02-10 10:56:12 +0000

    Created a Grid for the falling sand particles

---
+++ grid.py
@@ -0,0 +1,16 @@
+import pygame
+
+EMPTY_CELL_COLOR = (55,55,55)
+
+class Grid:
+    def __init__(self, width, height, cell_size):
+        self.rows = height // cell_size
+        self.columns = width // cell_size
+        self.cell_size = cell_size
+        self.cells = [[None for _ in range(self.columns)] for _ in range(self.rows)]
+
+    def draw(self, window):
+        for row in range(self.rows):
+            for column in range(self.columns):
+                pygame.draw.rect(window, EMPTY_CELL_COLOR,
+                    (column * self.cell_size, row * self.cell_size, self.cell_size - 1, self.cell_size - 1))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from grid import Grid
 
 #Simulation Defenition
 pygame.init()
@@ -7,15 +8,17 @@ pygame.init()
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
 MAX_FRAMERATE = 120
+CELL_SIZE = 20
+WINDOW_COLOR = (100, 29, 29)
 
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Falling Sand")
 
 clock = pygame.time.Clock()
+grid = Grid(WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE)
 
 #Simulation Loop
 while True:
-
     # 1. Event Handling
 
     for event in pygame.event.get():
@@ -26,6 +29,8 @@ while True:
     # 2. Update State
 
     # 3. Drawing
+    window.fill(WINDOW_COLOR)
+    grid.draw(window)
 
     pygame.display.flip()
     clock.tick(MAX_FRAMERATE)
